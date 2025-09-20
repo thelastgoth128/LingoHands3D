92916d75cc8034ff578e39430abf9b6a17ef4c62
thelastgoth128 <thindwacyrus86@gmail.com>
2025-09-20 23:55:18 +0200

    bone strcut

--- src/mesh.h
+++ src/mesh.h
@@ -3,12 +3,25 @@
 #include "shaders/shader.h"
 #include <vector>
 #include <string>
+#include "bone_info.h"
+#define MAX_BONE_INFLUENCE 4
 
 
 struct Vertex {
     glm::vec3 Position;
     glm::vec3 Normal;
     glm::vec2 TexCoords;
+
+    //tanget
+    glm::vec3 Tangent;
+    //bitangent
+    glm::vec3 Bitangent;
+
+    //bone indexe which will influe this vertex
+    int m_BoneIDs[MAX_BONE_INFLUENCE];
+    //weights from each bone
+    float m_Weights[MAX_BONE_INFLUENCE];
+
 };
 
 struct Texture{
